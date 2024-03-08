2dfbb20bc2945dc827eba5282d8f7d70f50d89f4
francogite <testupoad@sympatico.ca>
2024-03-08 16:22:33 -0500

    Exercise final

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',    
+    'djoser',
     'restaurant',
 ]
 
@@ -108,6 +110,29 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (  
+       'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '2/minute',
+        'user': '10/minute'
+    },
+    'DEFAULT_FILTER_BACKENDS': [
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2 
+}
+
+# add the following line
+DJOSER = {"USER_ID_FIELD":"username"}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Booking(models.Model):
-    id = models.SmallIntegerField(max_length=11, primary_key=True)
+    # id = models.SmallIntegerField(max_length=11, primary_key=True)
     name = models.CharField(max_length=255)
     no_of_guests = models.SmallIntegerField(max_length=6)
     bookingDate = models.DateTimeField(max_length=6)
@@ -11,10 +11,10 @@ class Booking(models.Model):
         return self.name
     
 class Menu(models.Model):
-    id = models.SmallIntegerField(max_length=5, primary_key=True)    
+    # id = models.SmallIntegerField(max_length=5, primary_key=True)    
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.SmallIntegerField(max_length=5)
     
-    def __str__(self):
-        return self.title
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
